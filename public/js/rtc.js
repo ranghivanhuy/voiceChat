f2b1da2bc617fb28027680cb478de5dda2d5b27b
vanbao120 <vanbao120@gmail.com>
2021-09-14 08:38:34 +0700

    14/09: add event btn share screen

--- public/js/rtc.js
+++ public/js/rtc.js
@@ -1,4 +1,7 @@
+import h from './helpers.js';
 window.addEventListener('load', () => {
+
+    var screen = '';
 
     document.getElementById('microphone').addEventListener('click', (e) => {
         e.preventDefault();
@@ -10,6 +13,57 @@ window.addEventListener('load', () => {
         e.preventDefault();
         document.querySelector('#mute-microphone').setAttribute('hidden', true);
         document.querySelector('#microphone').attributes.removeNamedItem('hidden');
+    });
+    function shareScreen() {
+        h.shareScreen().then((stream) => {
+            // h.toggleShareIcons(true);
+
+            //disable the video toggle btns while sharing screen. This is to ensure clicking on the btn does not interfere with the screen sharing
+            //It will be enabled was user stopped sharing screen
+            // h.toggleVideoBtnDisabled(true);
+
+            //save my screen stream
+            screen = stream;
+
+            //share the new stream with all partners
+            // broadcastNewTracks(stream, 'video', false);
+
+            //When the stop sharing button shown by the browser is clicked
+            screen.getVideoTracks()[0].addEventListener('ended', () => {
+                stopSharingScreen();
+            });
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
+
+    function stopSharingScreen() {
+        //enable video toggle btn
+        // h.toggleVideoBtnDisabled(false);
+
+        return new Promise((res, rej) => {
+            screen.getTracks().length ? screen.getTracks().forEach(track => track.stop()) : '';
+
+            res();
+        }).then(() => {
+            // h.toggleShareIcons(false);
+            // broadcastNewTracks(myStream, 'video');
+        }).catch((e) => {
+            console.error(e);
+        });
+    }
+
+    //When user clicks the 'Share screen' button
+    document.getElementById('share-screen').addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('more-display').setAttribute('hidden', true);
+        if (screen && screen.getVideoTracks().length && screen.getVideoTracks()[0].readyState != 'ended') {
+            stopSharingScreen();
+        }
+
+        else {
+            shareScreen();
+        }
     });
 
 });
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use('/public', express.static(path.join( __dirname, 'public')));
+let io = require('socket.io')(server);
+let stream = require('./public/js/stream');
+app.use('/public', express.static(path.join(__dirname, 'public')));
 var server = require('http').Server(app);
 server.listen(3001)
 
 app.get('/', (req, res) => {
-    res.sendFile( __dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
+
+io.of('/stream').on('connection', stream);

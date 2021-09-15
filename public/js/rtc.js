ee226414f9df16c6a81599c98c48d5d52000e999
Ho Van Huy <ranghivanhuy@gmail.com>
2021-09-15 11:22:18 +0700

    #20210915: create connection

--- public/js/rtc.js
+++ public/js/rtc.js
@@ -11,9 +11,225 @@ window.addEventListener('load', () => {
     } else {
         document.querySelector('body').setAttribute('class', 'background-room');
         document.querySelector('#main-container').attributes.removeNamedItem('hidden');
-    }
 
-    var screen = '';
+        var pc = [];
+
+        let socket = io('/stream');
+        var socketId = '';
+        var randomNumber = `__${h.generateRandomString()}`;
+        var myStream = '';
+        var screen = '';
+        var recordedStream = [];
+        var mediaRecorder = '';
+
+        //Get user video by default
+        getAndSetUserStream();
+
+        socket.on('connect', () => {
+            //set socketId
+            socketId = socket.io.engine.id;
+
+
+            socket.emit('subscribe', {
+                room: room,
+                socketId: socketId
+            });
+
+
+            socket.on('new user', (data) => {
+                socket.emit('newUserStart', { to: data.socketId, sender: socketId });
+                pc.push(data.socketId);
+                init(true, data.socketId);
+            });
+
+
+            socket.on('newUserStart', (data) => {
+                pc.push(data.sender);
+                init(false, data.sender);
+            });
+
+
+            socket.on('ice candidates', async (data) => {
+                data.candidate ? await pc[data.sender].addIceCandidate(new RTCIceCandidate(data.candidate)) : '';
+            });
+
+
+            socket.on('sdp', async (data) => {
+                if (data.description.type === 'offer') {
+                    data.description ? await pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description)) : '';
+
+                    h.getUserFullMedia().then(async (stream) => {
+                        if (!document.getElementById('local').srcObject) {
+                            h.setLocalStream(stream);
+                        }
+
+                        //save my stream
+                        myStream = stream;
+
+                        stream.getTracks().forEach((track) => {
+                            pc[data.sender].addTrack(track, stream);
+                        });
+
+                        let answer = await pc[data.sender].createAnswer();
+
+                        await pc[data.sender].setLocalDescription(answer);
+
+                        socket.emit('sdp', { description: pc[data.sender].localDescription, to: data.sender, sender: socketId });
+                    }).catch((e) => {
+                        console.error(e);
+                    });
+                }
+
+                else if (data.description.type === 'answer') {
+                    await pc[data.sender].setRemoteDescription(new RTCSessionDescription(data.description));
+                }
+            });
+
+            socket.on('chat', (data) => {
+                h.addChat(data, 'remote');
+            });
+
+        });
+
+        function getAndSetUserStream() {
+            h.getUserFullMedia().then((stream) => {
+                //save my stream
+                myStream = stream;
+
+                h.setLocalStream(stream);
+            }).catch((e) => {
+                console.error(`stream error: ${e}`);
+            });
+        }
+
+
+        function sendMsg(msg) {
+            let regetUsername = sessionStorage.getItem('username');
+            let data = {
+                room: room,
+                msg: msg,
+                sender: `${regetUsername} (${randomNumber})`
+            };
+
+            //emit chat message
+            socket.emit('chat', data);
+
+            //add localchat
+            h.addChat(data, 'local');
+        }
+
+
+
+        function init(createOffer, partnerName) {
+            pc[partnerName] = new RTCPeerConnection(h.getIceServer());
+
+            if (screen && screen.getTracks().length) {
+                screen.getTracks().forEach((track) => {
+                    pc[partnerName].addTrack(track, screen);//should trigger negotiationneeded event
+                });
+            }
+
+            else if (myStream) {
+                myStream.getTracks().forEach((track) => {
+                    pc[partnerName].addTrack(track, myStream);//should trigger negotiationneeded event
+                });
+            }
+
+            else {
+                h.getUserFullMedia().then((stream) => {
+                    //save my stream
+                    myStream = stream;
+
+                    stream.getTracks().forEach((track) => {
+                        pc[partnerName].addTrack(track, stream);//should trigger negotiationneeded event
+                    });
+
+                    h.setLocalStream(stream);
+                }).catch((e) => {
+                    console.error(`stream error: ${e}`);
+                });
+            }
+
+
+
+            //create offer
+            if (createOffer) {
+                pc[partnerName].onnegotiationneeded = async () => {
+                    let offer = await pc[partnerName].createOffer();
+
+                    await pc[partnerName].setLocalDescription(offer);
+
+                    socket.emit('sdp', { description: pc[partnerName].localDescription, to: partnerName, sender: socketId });
+                };
+            }
+
+
+
+            //send ice candidate to partnerNames
+            pc[partnerName].onicecandidate = ({ candidate }) => {
+                socket.emit('ice candidates', { candidate: candidate, to: partnerName, sender: socketId });
+            };
+
+
+
+            //add
+            pc[partnerName].ontrack = (e) => {
+                let str = e.streams[0];
+                if (document.getElementById(`${partnerName}-video`)) {
+                    document.getElementById(`${partnerName}-video`).srcObject = str;
+                }
+
+                else {
+                    //video elem
+                    let newVid = document.createElement('video');
+                    newVid.id = `${partnerName}-video`;
+                    newVid.srcObject = str;
+                    newVid.autoplay = true;
+                    newVid.className = 'remote-video';
+
+                    //video controls elements
+                    let controlDiv = document.createElement('div');
+                    controlDiv.className = 'remote-video-controls';
+                    controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
+                        <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
+
+                    //create a new div for card
+                    let cardDiv = document.createElement('div');
+                    cardDiv.className = 'card card-sm';
+                    cardDiv.id = partnerName;
+                    cardDiv.appendChild(newVid);
+                    cardDiv.appendChild(controlDiv);
+
+                    //put div in main-section elem
+                    document.getElementById('videos').appendChild(cardDiv);
+
+                    h.adjustVideoElemSize();
+                }
+            };
+
+            pc[partnerName].onconnectionstatechange = (d) => {
+                switch (pc[partnerName].iceConnectionState) {
+                    case 'disconnected':
+                    case 'failed':
+                        h.closeVideo(partnerName);
+                        break;
+
+                    case 'closed':
+                        h.closeVideo(partnerName);
+                        break;
+                }
+            };
+
+            pc[partnerName].onsignalingstatechange = (d) => {
+                switch (pc[partnerName].signalingState) {
+                    case 'closed':
+                        h.closeVideo(partnerName);
+                        break;
+                }
+            };
+        }
+
+        var screen = '';
 
     document.getElementById('microphone').addEventListener('click', (e) => {
         e.preventDefault();
@@ -202,5 +418,6 @@ window.addEventListener('load', () => {
         //     document.getElementById('chatbox').setAttribute('hidden', true);
         // }
     });
-
+        
+    }
 });
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
 let io = require('socket.io')(server);
 let stream = require('./public/js/stream');
+var path = require('path');
 let favicon = require('serve-favicon');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/favicon.ico'));
-var server = require('http').Server(app);
-server.listen(3001)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.of('/stream').on('connection', stream);
+server.listen(3001)
